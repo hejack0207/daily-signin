4fbafd511574301b5dcd107bbc204839e2a38e9e
ding <playdingnow@gmail.com>
2017-11-01 22:35:10 +0800

    style(smzdm): rm error code

--- sites/smzdm.js
+++ sites/smzdm.js
@@ -84,7 +84,6 @@ const run = async () => {
   // login with retry
   await loginProcess(page);
 
-  // await page.waitForNavigation({ waitUntil });
   await page.screenshot({
     path: './dev-images/smzdm-after-login-navigation.png',
   });
