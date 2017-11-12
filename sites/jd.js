3169dddec736855aed02077eb02cf91565b1dc40
ding <playdingnow@gmail.com>
2017-11-12 16:13:49 +0800

    feat(jd): init puppeteer codes

--- sites/jd.js
+++ sites/jd.js
@@ -1,33 +1,40 @@
+const puppeteer = require('puppeteer');
+
 const config = require('../config');
-
-const Nightmare = require('nightmare');
-
-const nightmare = Nightmare(config.nightmare);
 
 const { urls: URLS, elements: ELES } = config.sites.jd;
 
-const run = () => {
-  const { username, password } = config.profile;
+const loginProcess = async (page) => {
+  await page.goto(URLS.home);
+};
 
-  return nightmare
-    .goto(URLS.home)
-    .wait(ELES.gotoLogin)
-    .click(ELES.gotoLogin)
-    .wait(ELES.usernameLoginButton)
-    .click(ELES.usernameLoginButton)
-    .wait(ELES.usernameInpu)
-    .type(ELES.usernameInput, username)
-    .type(ELES.passwordInput, password)
-    .click(ELES.loginButton)
-    .wait(ELES.checkinBtn)
-    .click(ELES.checkinBtn)
-    .wait(ELES.checkinSuccess)
-    // "签到成功" if success
-    .evaluate(selector => document.querySelector(selector).innerText, ELES.checkinSuccess)
-    .end();
+const run = async () => {
+  // const { username, password } = config.profile;
+  const browser = await puppeteer.launch(config.puppeteer);
+  const page = await browser.newPage();
+  await page.setViewport(config.puppeteer.viewport);
+
+  await loginProcess(page);
+
+  // return nightmare
+  //   .goto(URLS.home)
+  //   .wait(ELES.gotoLogin)
+  //   .click(ELES.gotoLogin)
+  //   .wait(ELES.usernameLoginButton)
+  //   .click(ELES.usernameLoginButton)
+  //   .wait(ELES.usernameInpu)
+  //   .type(ELES.usernameInput, username)
+  //   .type(ELES.passwordInput, password)
+  //   .click(ELES.loginButton)
+  //   .wait(ELES.checkinBtn)
+  //   .click(ELES.checkinBtn)
+  //   .wait(ELES.checkinSuccess)
+  //   // "签到成功" if success
+  //   .evaluate(selector => document.querySelector(selector).innerText, ELES.checkinSuccess)
+  //   .end();
+  await browser.close();
 };
 
 module.exports = {
   run,
 };
-
